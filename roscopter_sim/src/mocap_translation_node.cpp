42a5b3d894e121ea4ab63e7e64f18a1ba9845c5e
Ian Reid <ian.young.reid@gmail.com>
2024-10-01 11:40:35 -0600

    Adjusted the mocap_translation_node

--- roscopter_sim/src/mocap_translation_node.cpp
+++ roscopter_sim/src/mocap_translation_node.cpp
@@ -3,6 +3,7 @@
 #include "Eigen/Geometry"
 #include "geometry_msgs/msg/vector3_stamped.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
+#include <rclcpp/timer.hpp>
 #include <sensor_msgs/msg/imu.hpp>
 #include "nav_msgs/msg/odometry.hpp"
 #include "rclcpp/rclcpp.hpp"
@@ -23,19 +24,40 @@ public:
     qos_profile.reliability(RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT);
 
     mocap_subscription_ = this->create_subscription<geometry_msgs::msg::PoseStamped>(
-        "vrpn_mocap/DemoQuad/pose", qos_profile, std::bind(&mocap_translation::publish_truth, this, _1)); // z is east, x is north, y is up 
+        "vrpn_mocap/JACOBflight/pose", qos_profile, std::bind(&mocap_translation::mocap_truth_callback, this, _1)); // z is east, x is north, y is up 
     imu_subscription_ = this->create_subscription<sensor_msgs::msg::Imu>(
         "imu/data", 10, std::bind(&mocap_translation::imu_callback, this, _1)); // z is east, x is north, y is up 
+    firmware_estimator_sub_ = this->create_subscription<geometry_msgs::msg::Vector3Stamped>("attitude/euler", 10, std::bind(&mocap_translation::firmware_estimator_callback, this, _1));
 
     publisher_ = this->create_publisher<roscopter_msgs::msg::State>("state", 10);
     // TODO: add GPS, baro and mag spoofed sensors.
+
+    float frequency = 100.0;
+    
+    auto update_period_ = std::chrono::microseconds(static_cast<long long>(1.0 / frequency * 1'000'000));
+    update_timer_ = this->create_wall_timer(update_period_, std::bind(&mocap_translation::publish_truth, this));
   }
 
 private:
   rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr mocap_subscription_;
   rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_subscription_;
+  rclcpp::Subscription<geometry_msgs::msg::Vector3Stamped>::SharedPtr firmware_estimator_sub_;
 
   rclcpp::Publisher<State>::SharedPtr publisher_;
+  
+  rclcpp::TimerBase::SharedPtr update_timer_;
+
+  void mocap_truth_callback(const geometry_msgs::msg::PoseStamped & msg)
+  {
+    mocap_truth_ = msg;
+  }
+
+  void firmware_estimator_callback(const geometry_msgs::msg::Vector3Stamped & msg)
+  {
+    rpy_estimated_.vector.set__x(msg.vector.x);
+    rpy_estimated_.vector.set__y(msg.vector.y);
+    rpy_estimated_.vector.set__z(msg.vector.z);
+  }
 
   //TODO insert wind callback.
   double wn = 0.0;
@@ -50,7 +72,10 @@ private:
   double p_e_prev = 0.0;
   double p_d_prev = 0.0;
 
-  void publish_truth(const geometry_msgs::msg::PoseStamped & msg)
+  geometry_msgs::msg::Vector3Stamped rpy_estimated_;
+  geometry_msgs::msg::PoseStamped mocap_truth_;
+
+  void publish_truth()
   {
 
     roscopter_msgs::msg::State state;
@@ -62,9 +87,9 @@ private:
     state.initial_lon = 0.0; // TODO implement correct initial lat and lon
     state.initial_alt = 0.0;
 
-    state.position[0] = msg.pose.position.x;
-    state.position[1] = msg.pose.position.z;
-    state.position[2] = -msg.pose.position.y;
+    state.position[0] = mocap_truth_.pose.position.x;
+    state.position[1] = mocap_truth_.pose.position.z;
+    state.position[2] = -mocap_truth_.pose.position.y;
 
     v_n = state.position[0] - p_n_prev; 
     v_n /= 2.0;
@@ -78,10 +103,10 @@ private:
     p_d_prev = state.position[2];
 
     Eigen::Quaternionf q;
-    q.w() = msg.pose.orientation.w;
-    q.x() = msg.pose.orientation.x;
-    q.y() = msg.pose.orientation.y;
-    q.z() = msg.pose.orientation.z;
+    q.w() = mocap_truth_.pose.orientation.w;
+    q.x() = mocap_truth_.pose.orientation.x;
+    q.y() = mocap_truth_.pose.orientation.z;
+    q.z() = -mocap_truth_.pose.orientation.y;
 
     state.inclination = 0.0;
 
@@ -92,9 +117,9 @@ private:
     euler(2) = atan2(2.0 * (q.w() * q.z() + q.x() * q.y()),
                      pow(q.w(), 2) + pow(q.x(), 2) - pow(q.y(), 2) - pow(q.z(), 2));
 
-    state.phi = euler(0);
-    state.theta = euler(1);
-    state.psi = euler(2);
+    state.phi = rpy_estimated_.vector.x;
+    state.theta = rpy_estimated_.vector.y;
+    state.psi = rpy_estimated_.vector.z;
 
     // Eigen::Vector3f body_frame_velocity;
     // body_frame_velocity << msg.twist.twist.linear.x, msg.twist.twist.linear.y, msg.twist.twist.linear.z;
@@ -124,10 +149,10 @@ private:
 
     state.quat_valid = true;
 
-    state.quat[0] = msg.pose.orientation.w;
-    state.quat[1] = msg.pose.orientation.x;
-    state.quat[2] = msg.pose.orientation.y;
-    state.quat[3] = msg.pose.orientation.z;
+    state.quat[0] = mocap_truth_.pose.orientation.w;
+    state.quat[1] = mocap_truth_.pose.orientation.x;
+    state.quat[2] = mocap_truth_.pose.orientation.z;
+    state.quat[3] = -mocap_truth_.pose.orientation.y;
 
     publisher_->publish(state);
   }
